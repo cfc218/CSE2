6cbcc40ba86c5311b9308deffd4baf10d99edecd
cfc218 <cfc218@lehigh.edu>
2015-08-28 12:30:35 +0000

    updated helloworld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,29 @@
+//Christopher Collado
+//Hello World Program
+
+// compile: javac HelloWorld.java
+// run:     java Hello World
+
+//define class
+public class HelloWorld {
+    
+//main method 
+    public static void main(String[] args) {
+        
+//print out Hello World with box around it
+        System.out.println("-----------------------");
+        System.out.println("--                  ---");
+        System.out.println("--   Hello World    ---");
+        System.out.println("--  *globe emoji*   ---");
+        System.out.println("--                  ---");
+        System.out.println("--                  ---");
+        System.out.println("-----------------------");
+        System.out.println("        TEST           ");
+    
+    //last line tells me where the program ran to
+    //if it shows, then it got to x point (x being wherever the TEST was placed which
+    //in this case is the end of the box
+        
+    }
+    
+}

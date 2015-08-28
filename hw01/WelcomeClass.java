4cf75816b9a50718228f9955d16dca222a4297ca
cfc218 <cfc218@lehigh.edu>
2015-08-28 12:57:49 +0000

    Welcome Class First HW

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,27 @@
+//HW01
+//Christopher Collado
+//defines class
+public class WelcomeClass {
+    //defines main method
+    public static void main(String[] args) {
+        //print statements, will print out welcome box then username sign
+        System.out.println("    --------------------");
+        System.out.println("    |      Welcome     |");
+        System.out.println("    --------------------");
+        System.out.println(" ");
+        System.out.println("   ^   ^   ^   ^   ^   ^");
+        System.out.println("  / \\ / \\ / \\ / \\ / \\ / \\ ");
+        System.out.println(" <-C---F---C---2---1---8->");
+        System.out.println("  \\ / \\ / \\ / \\ / \\ / \\ / ");
+        System.out.println("   v   v   v   v   v   v ");
+        System.out.println(" ");
+        //if print statement makes it here it succesfully printed out the box 
+        System.out.println(" TEST 1 ");
+        
+        //print statements here will print out little autobiographical bio/blurb
+        System.out.println("I'm a triplet and Spanish was our first language");
+        //if print statement makes it here it succesfully printed out the blurb 
+        System.out.println(" TEST 2 ");
+    }
+    
+}
